acf4cc6258ee221ba7c0579948a200bfa6f30cf5
David Lukava <dflukava@gmail.com>
2018-10-25 12:02:57 +0300

    Create webRequest.js

---
+++ webRequest.js
@@ -0,0 +1,6 @@
+chrome.webRequest.onBeforeRequest.addListener(
+        function(details) {
+          return {cancel: details.url.indexOf("://www.evil.com/") != -1};
+        },
+        {urls: ["<all_urls>"]},
+        ["blocking"]);
